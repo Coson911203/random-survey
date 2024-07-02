7a1ef57dd177c4b62988cb40602c04cf2eca674a
Coson911203 <0986561203coson@gmail.com>
2024-07-03 02:01:32 +0800

    Update url.js

--- url.js
+++ url.js
@@ -1,5 +1,4 @@
 const urls = [
     'https://docs.google.com/forms/d/1LAhcLgbWXo4KG_SAK_RTCDnvfOVD9d1dmoH5Ijfu1ZA/edit?ts=6651f454#responses',
-    'https://docs.google.com/forms/d/1UBcu0_qUelTCcBRcOYQRNBtvzF_WN95x2YvYH7BdBOc/edit?ts=6651f470#responses',
     'https://docs.google.com/forms/d/1fF7DevlO57eb9OhvXxMUKFhYTcSXk8zopR9Lln74cZ4/edit?ts=6651f47d#responses'
 ];
